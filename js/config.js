d9930859790e997c61b7ed024357c27f44625f4d
Vladimir Bozhkov <vladibo13@gmail.com>
2025-03-12 13:34:42 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_C1tjx4ElG', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '76s46s4c2606e956lb3tsqbp4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
